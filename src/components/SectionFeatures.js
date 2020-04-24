1186ddbb90ea4a27bdb96017435d795551abe1c1
Keren Megory-Cohen <keren@leftfieldlabs.com>
2020-04-24 00:57:44 -0700

    Adds temp 3d rendering

--- src/components/SectionFeatures.js
+++ src/components/SectionFeatures.js
@@ -9,22 +9,23 @@ export default class SectionFeatures extends React.Component {
         let section = _.get(this.props, 'section');
         return (
             <section id={_.get(section, 'section_id')} className={'block features-block bg-' + _.get(section, 'background') + ' outer'}>
+              <iframe id="3f15feea-4efa-4396-860c-dad2c7192481" src="https://www.vectary.com/embed/viewer/v1/viewer.html?model=3f15feea-4efa-4396-860c-dad2c7192481&env=studio1&envRotation=0&autoplay=1&turntable=0.5&showPreloader=1" frameborder="0" width="100%" height="480"></iframe>
               <div className="block-header inner-small">
-                {_.get(section, 'title') && 
+                {_.get(section, 'title') &&
                 <h2 className="block-title">{_.get(section, 'title')}</h2>
                 }
-                {_.get(section, 'subtitle') && 
+                {_.get(section, 'subtitle') &&
                 <p className="block-subtitle">
                   {htmlToReact(_.get(section, 'subtitle'))}
                 </p>
                 }
               </div>
-              {_.get(section, 'features') && 
+              {_.get(section, 'features') &&
               <div className="inner">
                 {_.map(_.get(section, 'features'), (feature, feature_idx) => (
                 <div key={feature_idx} className="block-item">
                   <div className="grid">
-                    {_.get(feature, 'image') && 
+                    {_.get(feature, 'image') &&
                     <div className="cell block-preview">
                       <img src={safePrefix(_.get(feature, 'image'))} alt={_.get(feature, 'title')} />
                     </div>
@@ -34,7 +35,7 @@ export default class SectionFeatures extends React.Component {
                       <div className="block-copy">
                         {markdownify(_.get(feature, 'content'))}
                       </div>
-                      {_.get(feature, 'actions') && 
+                      {_.get(feature, 'actions') &&
                         <CtaButtons {...this.props} actions={_.get(feature, 'actions')} />
                       }
                     </div>
