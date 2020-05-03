b9235077873320859e763ecda5deed8fdcf9fd1d
Keren Megory-Cohen <keren@leftfieldlabs.com>
2020-05-03 01:56:00 -0700

    Version/upgrade (#7)
    * Updates hero
    
    * Adds svg bottom swoosh
    
    * Updates removing images
    
    * Adds video and fixes features section
    
    * Adds temp fix to vimeo player responsiveness
    
    * Adds fade while you scroll
    
    * Adds reveal elements js
    
    * Adds smooth scroll

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,38 +1,91 @@
 import React from 'react';
-import {Helmet} from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import _ from 'lodash';
-import "../sass/main.scss"
+import reveal from '../../static/js/utils/reveal-elements';
+import '../sass/main.scss';
 
 //import {safePrefix} from '../utils';
 import Header from './Header';
 import Footer from './Footer';
 
 export default class Body extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <Helmet>
-                    <title>{_.get(this.props, 'pageContext.site.siteMetadata.title')}</title>
-                    <meta property="og:title" content={_.get(this.props, 'pageContext.site.siteMetadata.title')} />
-                    <meta charSet="utf-8"/>
-                    <meta name="viewport" content="width=device-width, initialScale=1.0" />
-                    <meta name="description" content={_.get(this.props, 'pageContext.site.siteMetadata.description')} />
-                    <meta property="og:description" content={_.get(this.props, 'pageContext.site.siteMetadata.description')} />
-                    <meta name="google" content="notranslate" />
-                    <link href="https://fonts.google.com/specimen/Open+Sans?selection.family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800" rel="stylesheet"/>
-                    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"></link>
-                    <link rel="shortcut icon" href="/images/logo-meta.png" type="image/x-icon" />
-                    {/* TODO: Uncomment when deploying app */}
-                    {/* <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/> */}
-                </Helmet>
-                <div id="page" className={'site palette-' + _.get(this.props, 'pageContext.site.siteMetadata.palette')}>
-                  <Header {...this.props} />
-                  <main id="content" className="site-content">
-                    {this.props.children}
-                  </main>
-                  <Footer {...this.props} />
-                </div>
-            </React.Fragment>
-        );
-    }
+  async componentDidMount() {
+    reveal([
+      'h1',
+      'h2',
+      'h3',
+      'h4',
+      'h5',
+      'h6',
+      'p',
+      'li',
+      'a',
+      'img',
+      'span',
+      'button',
+      'svg',
+    ]);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Helmet>
+          <title>
+            {_.get(this.props, 'pageContext.site.siteMetadata.title')}
+          </title>
+          <meta
+            property='og:title'
+            content={_.get(this.props, 'pageContext.site.siteMetadata.title')}
+          />
+          <meta charSet='utf-8' />
+          <meta
+            name='viewport'
+            content='width=device-width, initialScale=1.0'
+          />
+          <meta
+            name='description'
+            content={_.get(
+              this.props,
+              'pageContext.site.siteMetadata.description'
+            )}
+          />
+          <meta
+            property='og:description'
+            content={_.get(
+              this.props,
+              'pageContext.site.siteMetadata.description'
+            )}
+          />
+          <meta name='google' content='notranslate' />
+          <link
+            href='https://fonts.google.com/specimen/Open+Sans?selection.family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800'
+            rel='stylesheet'
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Montserrat&display=swap'
+            rel='stylesheet'></link>
+          <link
+            rel='shortcut icon'
+            href='/images/logo-meta.png'
+            type='image/x-icon'
+          />
+          {/* TODO: Uncomment when deploying app */}
+          {/* <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/> */}
+        </Helmet>
+        <div
+          id='page'
+          className={
+            'site palette-' +
+            _.get(this.props, 'pageContext.site.siteMetadata.palette')
+          }>
+          <Header {...this.props} />
+          <main id='content' className='site-content'>
+            {this.props.children}
+          </main>
+          <Footer {...this.props} />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
---
+++ static/js/utils/scaffold-utils.js
@@ -0,0 +1,160 @@
+
+export const qs = (string, el = document) => el.querySelector(string);
+export const qTag = (string, el = document) =>
+  el.getElementsByTagName(string);
+export const qsAll = (string, el = document) => el.querySelectorAll(string);
+export const getElementDimensions = el => ({
+  width: el.offsetWidth,
+  height: el.offsetHeight,
+});
+
+export const getScreen = () => ({
+  scrollY: window.scrollY,
+  width: window.innerWidth,
+  height: window.innerHeight,
+});
+
+export const getScroll = () => ({
+  scrollY: window.scrollY || window.pageYOffset,
+  deltaY: 0,
+});
+
+export const forEachPollyfill = () => {
+  if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = function(callback, thisArg) {
+        thisArg = thisArg || window;
+        for (let i = 0; i < this.length; i++) {
+            callback.call(thisArg, this[i], i, this);
+        }
+    };
+  }
+};
+
+export const getClientRect = el => {
+  const d = el.getBoundingClientRect();
+  return {
+    left: d.left || d.x,
+    right: d.right || d.y,
+    x: d.x || d.left,
+    y: d.y || d.top,
+    top: d.top,
+    bottom: d.bottom,
+    width: d.width,
+    height: d.height,
+  };
+};
+
+export const isFunction = function(obj) {
+  return typeof obj == 'function' || false;
+};
+
+/*************
+    VIEWPORT
+*************/
+
+export const isMobileViewport = () => getScreen().width < 600;
+export const isMediumViewport = () =>
+  getScreen().width > 600 && getScreen().width < 992;
+export const isLargeViewport = () => getScreen().width > 1200;
+export const isSmallHeight = () => getScreen().height < 480;
+
+const FULLY_SCREEN_PERCENT = 0.25;
+const PARTIALLY_SCREEN_PERCENT = 0.82;
+const JUST_PARTIALLY_SCREEN_PERCENT = 0.98;
+
+export const isElementInView = (domRect, screen) => {
+  const fullyS = screen.height * FULLY_SCREEN_PERCENT;
+  const halfS = screen.height * PARTIALLY_SCREEN_PERCENT;
+  const partiallyS = screen.height * PARTIALLY_SCREEN_PERCENT;
+  const justS = screen.height * JUST_PARTIALLY_SCREEN_PERCENT;
+  return {
+    x: domRect.x,
+    y: domRect.y,
+    width: domRect.width,
+    height: domRect.height,
+    fully: Math.abs(domRect.y) >= 0 && Math.abs(domRect.y) <= fullyS,
+    half: Math.abs(domRect.y) > 0 && Math.abs(domRect.y) < halfS,
+    partially: Math.abs(domRect.y) > 0 && Math.abs(domRect.y) < partiallyS,
+    entering:
+      Math.abs(domRect.y) < screen.height && Math.abs(domRect.y) > partiallyS,
+    justEntering:
+      Math.abs(domRect.y) < screen.height && Math.abs(domRect.y) > justS,
+    inactive: Math.abs(domRect.y) > screen.height - 1,
+  };
+};
+
+export const clamp = (val, min, max) => Math.max(min, Math.min(val, max));
+export const mapRange = (value, inMin, inMax, outMin, outMax) =>
+  (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+
+const div = document.createElement('div');
+const prefixes = ['Webkit', 'Moz', 'O', 'ms'];
+export const prefixStyle = prop => {
+  const style = div.style;
+  if (prop in style) {
+    return prop;
+  }
+  const titleCase = prop.charAt(0).toUpperCase() + prop.slice(1);
+  for (let i = prefixes.length; i >= 0; i--) {
+    const name = prefixes[i] + titleCase;
+    if (name in style) {
+      return name;
+    }
+  }
+  return false;
+};
+
+export const scale = (
+  parentWidth,
+  parentHeight,
+  childWidth,
+  childHeight,
+  contains
+) => {
+  const doRatio = childWidth / childHeight;
+  const cRatio = parentWidth / parentHeight;
+  let width = parentWidth;
+  let height = parentHeight;
+
+  if (contains ? doRatio > cRatio : doRatio < cRatio) {
+    height = width / doRatio;
+  } else {
+    width = height * doRatio;
+  }
+
+  return {
+    width,
+    height,
+    x: (parentWidth - width) / 2,
+    y: (parentHeight - height) / 2,
+  };
+};
+
+/**
+ * Is el scrolled into view?
+ * @param {!Element} el
+ * @param {boolean} partial True for partialy visible, false for
+*  completely visibile.
+ * @param {!Function} callback Do this if in view.
+ */
+export const whenScrolledIntoView = function(el, partial, callback) {
+  const listenerFunc = () => {
+    const rect = el.getBoundingClientRect();
+    const elemTop = rect.top;
+    const elemBottom = rect.bottom;
+
+    // Only completely visible elements return true:
+    let isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    if (partial) {
+      // Partially visible elements return true:
+      isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+    }
+    if (isVisible) {
+      callback();
+      window.removeEventListener('scroll', listenerFunc);
+    }
+  };
+
+  listenerFunc();
+  window.addEventListener('scroll', listenerFunc);
+};
